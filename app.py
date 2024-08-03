0521d58f052386cced97bbc3a1e185cb5a3220ef
Shuaibu Sani Barde <100693744+Barde-S@users.noreply.github.com>
2024-08-03 20:46:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -303,9 +303,9 @@ if selected == "Static":
 
     # Convert the Series to a DataFrame for easier plotting with Plotly
     total_allocations_by_state_df = total_allocations_by_state.reset_index()
-    total_allocations_by_state_df.columns = ['LGC', 'Total Allocation']
+    # total_allocations_by_state_df.columns = ['LGC', 'Total Allocation']
     total_allocations_by_state_df['y'] =  ((total_allocations_by_state_df['Total Allocation'])/1000)
-    (total_allocations_by_state_df.y)/1000
+    # (total_allocations_by_state_df.y)/1000
 
     
     # Create the bar chart using Plotly
