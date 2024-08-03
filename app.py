400bd76322702d9b394d927a3ed90ac5c6b2a69f
Shuaibu Sani Barde <100693744+Barde-S@users.noreply.github.com>
2024-08-03 09:53:20 +0100

    Update app.py just imported the go module of plotly

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+import plotly.graph_objects as go
 from streamlit_option_menu import option_menu
 import numpy as np
 regions = {
